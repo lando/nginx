040f715fd7e46d4e9d5a71c38b2c18a0eecc677f
Mike Pirog <mike@lando.dev>
2022-02-26 20:09:42 -0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,13 +26,13 @@ module.exports = {
     sidebar: [
       {
         text: 'Getting Started',
-        link: '/index.md',
+        link: '/index.html',
       },
-      '/config.md',
-      '/support.md',
+      '/config.html',
+      '/support.html',
       {text: 'Examples', link: 'https://github.com/lando/nginx/tree/main/examples'},
       {text: 'Release Notes', link: 'https://github.com/lando/nginx/releases'},
-      '/development.md',
+      '/development.html',
     ],
   },
 };
